fac2755cf3d936ee6c604e9835a0bcc0117f28ca
rickkun11 <rickkun11@gmail.com>
2023-07-23 06:06:02 +0700

    add desc all role

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -13,9 +13,13 @@ const AdminDashboard = () => {
           </div>
           <div className="col-md-9">
             <div className="card w-75 p-3">
-              <h3> Admin Name : {auth?.user?.name}</h3>
-              <h3> Admin Email : {auth?.user?.email}</h3>
-              <h3> Admin Contact : {auth?.user?.phone}</h3>
+            <h3 align="center">Information</h3>
+              <span><h3>Name      : {auth?.user?.name}</h3></span>
+              <span><h3>Email     : {auth?.user?.email}</h3></span>
+              <span><h3>Address    : {auth?.user?.address}</h3></span>
+              <span><h3>Phone : {auth?.user?.phone}</h3></span>
+              <span><h3>Role : Admin</h3></span>
+
             </div>
           </div>
         </div>
--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -13,9 +13,12 @@ const Dashboard = () => {
           </div>
           <div className="col-md-9">
             <div className="card w-75 p-3">
-              <h3>{auth?.user?.name}</h3>
-              <h3>{auth?.user?.email}</h3>
-              <h3>{auth?.user?.address}</h3>
+            <h3 align="center">Information</h3>
+              <span><h3>Name      : {auth?.user?.name}</h3></span>
+              <span><h3>Email     : {auth?.user?.email}</h3></span>
+              <span><h3>Address    : {auth?.user?.address}</h3></span>
+              <span><h3>Phone : {auth?.user?.phone}</h3></span>
+              <span><h3>Role : User</h3></span>
             </div>
           </div>
         </div>
