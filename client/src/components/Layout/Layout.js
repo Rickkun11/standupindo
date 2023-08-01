be67fa647e98934048dd59743d8a0f5b6e7f1d58
rickkun11 <rickkun11@gmail.com>
2023-08-01 11:17:01 +0700

    scale footer

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -14,7 +14,7 @@ const Layout = ({ children, title, description, keywords, author }) => {
         <title>{title}</title>
       </Helmet>
       <Header />
-      <main style={{ minHeight: "80.6vh" }}>
+      <main style={{ minHeight: "81vh" }}>
         <Toaster />
 
         {children}
